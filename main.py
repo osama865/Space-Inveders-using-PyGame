e5bc7a058d19750cae53a56e530abd43d25061f7
osama <osama2002ibrahim@gmail.com>
2020-10-07 05:29:54 +0200

    adding git ignore

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import time
 import random
 
+# Load images
 Red_Ship = pygame.image.load(os.path.join('assests' , 'ship_red_small.png'))
 Blue_Ship = pygame.image.load(os.path.join('assests' , 'ship_blue_small.png'))
 Green_Ship = pygame.image.load(os.path.join('assests' , 'ship_green_small.png'))
