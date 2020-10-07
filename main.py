bde500f27daf6b1699f12197551aed4c675171c2
osama <osama2002ibrahim@gmail.com>
2020-10-07 05:16:14 +0200

    starting the game

---
+++ main.py
@@ -0,0 +1,13 @@
+# Space invedres using pygame
+
+import pygame
+import os
+import time
+import random
+
+Red_Ship = pygame.image.load(os.path.join('assests' , 'ship_red_small.png'))
+Blue_Ship = pygame.image.load(os.path.join('assests' , 'ship_blue_small.png'))
+Green_Ship = pygame.image.load(os.path.join('assests' , 'ship_green_small.png'))
+
+# Main player
+Yellow_Ship = pygame.image.load(os.path.join('assests' , 'ship_yellow.png'))
